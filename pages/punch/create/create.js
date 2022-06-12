fcdc7cf68661bbd9b7e6785015e65077e8d337c3
DL <601209253@qq.com>
2022-06-12 18:35:06 +0800

    demo based on git:CardOnePersion

---
+++ pages/punch/create/create.js
@@ -0,0 +1,171 @@
+var util = require('../../../utils/util.js');
+
+Page({
+  data: {
+    title: '',
+    content: '坚持打卡~',
+    bIncludeToday: true,
+    sumDays: 21
+  },
+
+  // 加载时初始化一下  时间 
+  onLoad() {
+
+    var date = this.getDate(this.data.bIncludeToday);
+
+    this.setData({
+      createTime: util.formatTime(new Date()),
+      beginDate: date.beginDate,
+      endDate: date.endDate
+    })
+  },
+
+  // 获取 起止时间
+  getDate(bIncludeToday) {
+
+    var now = new Date(),
+      beginDate = bIncludeToday ? util.formatFutureTime(now, 0) : util.formatFutureTime(now, 1),
+      endDate = util.formatFutureTime(beginDate, this.data.sumDays - 1);
+
+    return {
+      beginDate: beginDate,
+      endDate: endDate
+    };
+  },
+
+
+  // 是否包含今天
+  bindSwithChange(e) {
+
+    var bIncludeToday = e.detail.value;
+    var date = this.getDate(bIncludeToday);
+
+    this.setData({
+      bIncludeToday: bIncludeToday,
+      beginDate: date.beginDate,
+      endDate: date.endDate
+    })
+  },
+
+  // 设置计划天数
+  sumDaysChange(e) {
+    var oriSumDays = this.data.sumDays;
+    var sumDays = parseInt(e.detail.value);
+    if (sumDays && sumDays > 0) {
+      this.setData({
+        sumDays: sumDays,
+        endDate: util.formatFutureTime(this.data.beginDate, sumDays - 1)
+      })
+    } else {
+      wx.showModal({
+        title: 'Hey, 别急',
+        content: '周期至少是1天',
+        showCancel: false
+      })
+      this.setData({
+        sumDays: oriSumDays
+      })
+    }
+  },
+
+  // 两种绑定函数的写法。
+  // 时间设定
+  // 怎么都觉得这里有些重复
+  bindBeginDateChange(e) {
+    var beginDate = e.detail.value,
+      now = new Date();
+
+    if (util.compareDate(beginDate, now) > 0) {
+      var endDate = util.formatFutureTime(beginDate, this.data.sumDays - 1);
+
+      this.setData({
+        beginDate: beginDate,
+        endDate: endDate
+      })
+    } else {
+      wx.showModal({
+        title: 'Hey, 别急',
+        content: '开始时间不得早于今天',
+        showCancel: false
+      })
+    }
+  },
+
+  bindEndDateChange: function(e) {
+
+    var endDate = e.detail.value,
+      beginDate = this.data.beginDate,
+      now = new Date();
+
+    if (util.compareDate(endDate, beginDate) > 0 && util.compareDate(endDate, now) > 0) {
+
+      var sumDays = util.getSumDays(beginDate, endDate);
+      this.setData({
+        sumDays: sumDays,
+        endDate: endDate
+      })
+    } else {
+      wx.showModal({
+        title: 'Hey, 别急',
+        content: '结束时间不得早于开始时间和今天',
+        showCancel: false
+      })
+    }
+  },
+
+
+  // 标题，简介 设定
+  titleChange(e) {
+    this.setData({
+      title: e.detail.value
+    })
+  },
+  contentChange(e) {
+    this.setData({
+      content: e.detail.value
+    })
+  },
+
+  // 创建新活动。创建成功跳转至创建成功页
+  createActivity() {
+
+    // 活动名称自然不可为空
+    if (this.data.title.length == 0) {
+
+      wx.showModal({
+        title: 'Hey, 别急',
+        content: '请先给计划起个名字',
+        showCancel: false
+      })
+      return;
+    }
+
+    // 数据保存
+    // 缓存中的数据类型是string  console.log(typeof(arr))
+    var arr = wx.getStorageSync('activity');
+    var data = [];
+    var maxID = -1;
+    if (arr.length) {
+      arr.forEach((item, i) => {
+        if (item.id > maxID)
+          maxID = item.id;
+        data.push(item);
+      })
+    }
+
+    // 新增数据跟在尾巴上
+    var id = maxID + 1;
+    this.setData({
+      id: id
+    })
+
+    data.push(this.data);
+    wx.setStorageSync('activity', data);
+    console.log(data)
+
+    // 页面跳转  关闭当前页面
+    wx.redirectTo({
+      url: '../detail/detail?id=' + id
+    })
+  }
+})
---
+++ pages/punch/detail/detail.js
@@ -0,0 +1,165 @@
+var util = require("../../../utils/util.js");
+
+Page({
+
+  data: {
+
+    // 活动状态 0(未开始) 1(进行中) 2(已结束)
+    state: 0,
+    btnState: '',
+    disabled: false,
+
+    // 打卡详情
+    punchCount: 0,
+    sumDays: 0,
+    activity: []
+  },
+
+  // 加载初始化
+  onLoad(e) {
+    this.initBaseData(e.id);
+    this.initPunchData(e.id);
+  },
+
+  // 基础数据
+  initBaseData(id) {
+    var arr = wx.getStorageSync('activity');
+
+    if (arr.length) {
+      arr.forEach((item) => {
+
+        if (item.id == id) {
+
+          var beginDate = item.beginDate;
+          var endDate = item.endDate;
+          var iTaskState = util.retTaskState(beginDate, endDate);
+          var bPunched = util.retPunched(item.id);
+
+          this.setData({
+            id: id,
+            title: item.title,
+            content: item.content,
+            createTime: item.createTime,
+            beginDate: beginDate,
+            endDate: endDate,
+
+            state: iTaskState,
+            stateColorClass: util.getTaskColorClass(iTaskState),
+            // btnState: 按钮文字
+            btnState: util.getBtnText(iTaskState, bPunched),
+
+            // 活动未开始 or 今天已打卡
+            disabled: iTaskState != 1 || bPunched,
+
+            sumDays: item.sumDays
+          })
+        }
+      })
+    }
+  },
+  // 打卡记录、勋章
+  initPunchData(id) {
+
+    var data = wx.getStorageSync('signin' + id);
+
+    // 每次打卡详细记录 data.arrRecord
+    var arrRecord = data.arrRecord ? data.arrRecord : [];
+
+    // 打卡情况总览 date.reconds
+    var records = data.records ? data.records : [];
+
+    // 已打卡天数
+    var punchCount = data.punchCount ? data.punchCount : 0;
+
+    // 最长累计打卡天数
+    var serialMaxDays = data.serialMaxDays ? data.serialMaxDays : 0;
+
+    // 打卡已持续天数 data.lastedDays
+    var endD = util.compareDate(this.data.endDate, new Date()) > 0 ? util.formatDate(new Date()) : this.data.endDate;
+    var lastedDays = util.getSumDays(this.data.beginDate, endD);
+
+    var d = [];
+    for (var i = 0; i < lastedDays; i++) {
+      if (records && records[i]) {
+        d.push(true);
+      } else {
+        d.push(false);
+      }
+    }
+    // console.log('打卡记录总览赋值', d);
+
+
+    this.setData({
+      punchCount: punchCount,
+      serialMaxDays: serialMaxDays,
+      activity: d,
+      arrRecord: arrRecord
+    })
+  },
+
+  // 编辑活动
+  editActivity() {
+
+    wx.redirectTo({
+      url: '../edit/edit?id=' + this.data.id +'&state='+this.data.state,
+    })
+  },
+
+  // 创建者结束活动
+  stopActivity() {
+
+    var that = this;
+    wx.showModal({
+      title: '提示',
+      content: '将删除相关数据，继续操作？',
+      showCancel: true,
+
+      success: function(res) {
+        if (res.confirm) {
+
+          that.DeleteActivity(that.data.id);
+          // 活动被销毁，返回main
+          wx.navigateBack({
+            url: '../main/main?update=true'
+          })
+        }
+      }
+    })
+  },
+
+  // 删除活动，
+  // 依据活动id  删除该id下的所有数据
+  // 单人的，删除自己的数据
+  // 多人的，仅删除自己的并结束活动？
+  DeleteActivity(id) {
+
+    // Del 这个活动 
+    var arr = wx.getStorageSync('activity');
+    var data = [];
+    if (arr.length) {
+      arr.forEach((item) => {
+        if (item.id != id) {
+          data.push(item);
+        }
+      })
+      wx.setStorageSync('activity', data);
+    }
+
+    // Del 这个活动下的打卡数据
+    wx.removeStorageSync('signin' + id);
+  },
+
+  // 这里签到。签到成功刷新当前页面
+  signIn() {
+
+    var id = this.data.id;
+    var bSucceed = util.signIn(id, this.data.beginDate);
+    if (bSucceed) {
+      this.initPunchData(id);
+      this.setData({
+        btnState: util.getBtnText(1, true),
+        disabled: true
+      })
+    }
+  }
+})
